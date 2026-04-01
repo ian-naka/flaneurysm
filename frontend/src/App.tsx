c61b10fa718461c4db7e88e34a833ca9ca762553
Ian Nakamura <ian.nakamura@estudante.ufjf.br>
2026-04-01 13:19:23 -0300

    Remoção de /criar-registro

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './pages/auth/Login';
-import CriarPostagem from './pages/postagem/CriarPostagem';
 import Dashboard from './pages/dashboard/Dashboard';
 import AdminHub from './pages/admin/AdminHub';
 import ProtectedRoute from './components/ProtectedRoute';
@@ -11,11 +10,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Dashboard />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/criar-registro" element={
-          <ProtectedRoute>
-            <CriarPostagem />
-          </ProtectedRoute>
-        } />
         <Route path="/admin" element={
           <ProtectedRoute>
             <AdminHub />
